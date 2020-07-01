0c3e330c625440ea079b6d314af6cedc29899171
rashtion <64362298+rashtion@users.noreply.github.com>
2020-07-01 11:47:49 +0300

    Delete information.py

--- information.py
+++
@@ -1,6 +0,0 @@
-# www.github.com/rashtion
-
-isim = input("What is your name? : ")
-print("Welcome the terminal werewolf,"+isim)
-print("If you dont know how to play this game ? please use the 'python3 howtoplay.py' have fun! ")
-print("Developer : github.com/rashtion")
