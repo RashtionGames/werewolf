c3323ac43675faabacfa2b0c5f4be1aecbaac4d5
rashtion <64362298+rashtion@users.noreply.github.com>
2020-07-01 11:40:13 +0300

    Add files via upload

---
+++ game.sh
@@ -0,0 +1,88 @@
+# This game's creator is rashtion = y. emre bakkal. You can't copy or delete this text.
+#!/data/data/com.termux/bin/bash
+#!/bin/bash
+pkg install toast
+clear
+echo "Welcome To WereWolf!"
+sleep 2
+echo "Please wait the download of the packages. If you want me to let you know when it's done download TERMUX: API."
+sleep 4
+pkg install termux-API
+sleep 1
+sleep 3
+termux-toast -b black -c white "You are using Termux API, I start downloading the packages."
+sleep 3
+termux-toast -b black -c white "Hey! dont scare, I'm not a hacker. I'm just a werewolf game's artificial intelligence"
+sleep 3
+termux-toast -b black -c white "Okay, we're starting. Confirm what appears on the screen so I can send you a notification when the packages are installed."
+sleep 3
+termux-setup-storage
+sleep 4
+termux-telephony-deviceinfo
+sleep 5
+echo "Installing the packages.."
+sleep 2
+termux-toast -b black -c white "Installation triggered, I'm starting."
+sleep 3
+
+
+apt update && apt upgrade -y
+  pkg instal python3 -y
+  pkg install python -y
+  pkg install python2 -y
+	pkg install git -y
+	pkg install php -y
+	pkg install perl -y
+	pkg install nano -y
+	pkg install python python2 -y
+	pkg install pip pip2 -y
+	pip install --upgrade pip
+	pip install bs4
+	pip install future
+	pip install requests
+	pip install colorama
+	pip install wordlist
+	pkg install termux-api -y
+	pkg install curl  -y
+	pkg install wget -y
+	pkg install neofetch -y
+	apt update && apt upgrade -y
+	chmod 777 tst.sh
+	pkg install unstable-repo -y
+	pkg install metasploit -y
+sleep 3
+ termux-notifaciton "The packages are installed, game is ready!"
+sleep 2
+echo "Connecting to the server.."
+sleep 2
+echo "Connecting to the server.."
+echo "Connecting to the server.."
+sleep 2
+echo "An error is a occured : Server Down"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#echo "Welcome to WereWolf terminal game!"
+#echo "Ouupss.. Im sorry, because the game in a maintenance."
---
+++ information.py
@@ -0,0 +1,6 @@
+# www.github.com/rashtion
+
+isim = input("What is your name? : ")
+print("Welcome the terminal werewolf,"+isim)
+print("If you dont know how to play this game ? please use the 'python3 howtoplay.py' have fun! ")
+print("Developer : github.com/rashtion")
